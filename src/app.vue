db6c849e43bc991e01f12b2021f902cb83e85870
孙志斌 <sunzhibin@corp.netease.com>
2021-08-17 20:57:49 +0800

    feat: test

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="table" v-if="show">
-    <span class="close" @click="handleClose">x</span>
+    <span class="close" @click="onClose">x</span>
     <div>{{ tradeList.length }}</div>
   </div>
-  <el-button class="btnHide" @click="handleOpen" v-else type="primary"
+  <el-button class="btnHide" @click="onOpen" v-else type="primary"
     >open</el-button
   >
 </template>
 <script>
 import "./app.less";
-import { proxy } from "ajax-hook";
+import { proxy, unProxy } from "ajax-hook";
 import $ from "jquery";
 
 export default {
@@ -20,102 +20,135 @@ export default {
     };
   },
   methods: {
-    handleOpen() {
-      this.show = true;
+    onInitProxy() {
+      // ajaxhook钩子，拦截请求数据
+      proxy({
+        //请求发起前进入
+        onRequest: (config, handler) => {
+          handler.next(config);
+        },
+        //请求发生错误时进入，比如超时；注意，不包括http状态码错误，如404仍然会认为请求成功
+        onError: (err, handler) => {
+          handler.next(err);
+        },
+        //请求成功后进入
+        onResponse: (response, handler) => {
+          const { config = {}, response: data = {} } = response || {};
+          const { url = "" } = config || {};
+          if (url.indexOf("/printSend.do?m=queryTrades") > -1) {
+            const { tradeList = [] } = JSON.parse(data).data || {};
+            this.tradeList = tradeList;
+            //给容器加上订单号,之后方便操作
+            if (tradeList.length > 0) {
+              setTimeout(() => {
+                $(".table-row.row-expand").attr("id", function() {
+                  //将订单货物的div也设置一个id,方便后续操作
+                  var searchOrderId = $(this)
+                    .next()
+                    .find(".table-column.trade-detail-column1:first")
+                    .find("a:first")
+                    .html();
+                  $(this)
+                    .next()
+                    .attr("id", "orderSku_" + searchOrderId);
+                  //将orderid属性等于纯文件
+                  $(this).attr("orderid", searchOrderId);
+                  return "orderInfo_" + searchOrderId;
+                });
+              }, 1000);
+            }
+          }
+          handler.next(response);
+        },
+      });
     },
-    handleClose() {
+    onGetOrderDetail(orderId) {
+      unProxy();
+      setTimeout(() => {
+        const id = `#orderInfo_${orderId}`;
+        console.log(
+          $(id)
+            .find("i[title='明文显示收件信息']")
+            .click()
+        );
+      }, 2000);
+    },
+    onOpen() {
+      this.onGetOrderDetail("1688776129161565806");
+      // this.show = true;
+    },
+    onClose() {
       this.show = false;
     },
   },
   mounted() {
-    setTimeout(() => {
-      // $.get("http://8.134.72.1/testCORS.php", (res) => {
-      //   console.log(res);
-      // });
-      // $.post(
-      //   "http://tc1.woda.com/printSend.do?m=batchSaveOutsid",
-      //   {
-      //     expressId: "50",
-      //     tableType: "0",
-      //     keyTidCtids: JSON.stringify({
-      //       "1345442199065381079": ["1345442199065381079"],
-      //     }),
-      //     expressTemplateId: "934771",
-      //     expressTemplateName: "韵达手动填单号",
-      //     expressTemplateKind: "0",
-      //     keyTidOutsid: JSON.stringify({
-      //       "1345442199065381079": "4315697655846",
-      //     }),
-      //     tidSelectedOids: JSON.stringify({
-      //       "1345442199065381079": [
-      //         "1345442199066381079",
-      //         "1345442199067381079",
-      //       ],
-      //     }),
-      //   },
-      //   (res) => {
-      //     let originData = list.find((item) => item.keyTid === id);
-      //     const { data = {} } = JSON.parse(res) || {};
-      //     Object.assign(originData, { ...data[id] });
-      //     ids.shift();
-      //     getItemData(ids);
-      //   }
-      // );
-      const data = {
-        params: {
-          keyTidOutsid: { "1345442199065381079": "4315697655846" },
-          tidSelectedOids: {
-            "1345442199065381079": [
-              "1345442199066381079",
-              "1345442199067381079",
-            ],
-          },
-          keyTidCtids: { "1345442199065381079": ["1345442199065381079"] },
-          tableType: 0,
-          templateId: 925325,
-          templateName: "韵达_一联_2",
-          templateType: 1,
-          templateKind: 1,
-          expressId: 50,
-          expressName: "韵达",
-          packageType: 0,
-          tidRowIndex: { "1345442199065381079": 1 },
-          tidJiazhuangMap: { "1345442199065381079": "" },
-          tidThreeplMap: { "1345442199065381079": {} },
-          sendType: "offline",
-        },
-        bodyRequest: 1,
-      };
-      $.ajax({
-        url: "http://tc1.woda.com/printSend.do?m=send",
-        type: "POST",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        data: JSON.stringify(data),
-      });
-    }, 5000);
-
-    // ajaxhook钩子，拦截请求数据
-    proxy({
-      //请求发起前进入
-      onRequest: (config, handler) => {
-        handler.next(config);
-      },
-      //请求发生错误时进入，比如超时；注意，不包括http状态码错误，如404仍然会认为请求成功
-      onError: (err, handler) => {
-        handler.next(err);
-      },
-      //请求成功后进入
-      onResponse: (response, handler) => {
-        const { config = {}, response: data = {} } = response || {};
-        const { url = "" } = config || {};
-        if (url.indexOf("/printSend.do?m=queryTrades") > -1) {
-          const { tradeList = [] } = JSON.parse(data).data || {};
-          this.tradeList = tradeList;
-        }
-        handler.next(response);
-      },
-    });
+    // setTimeout(() => {
+    // $.get("http://8.134.72.1/testCORS.php", (res) => {
+    //   console.log(res);
+    // });
+    // $.post(
+    //   "http://tc1.woda.com/printSend.do?m=batchSaveOutsid",
+    //   {
+    //     expressId: "50",
+    //     tableType: "0",
+    //     keyTidCtids: JSON.stringify({
+    //       "1345442199065381079": ["1345442199065381079"],
+    //     }),
+    //     expressTemplateId: "934771",
+    //     expressTemplateName: "韵达手动填单号",
+    //     expressTemplateKind: "0",
+    //     keyTidOutsid: JSON.stringify({
+    //       "1345442199065381079": "4315697655846",
+    //     }),
+    //     tidSelectedOids: JSON.stringify({
+    //       "1345442199065381079": [
+    //         "1345442199066381079",
+    //         "1345442199067381079",
+    //       ],
+    //     }),
+    //   },
+    //   (res) => {
+    //     let originData = list.find((item) => item.keyTid === id);
+    //     const { data = {} } = JSON.parse(res) || {};
+    //     Object.assign(originData, { ...data[id] });
+    //     ids.shift();
+    //     getItemData(ids);
+    //   }
+    // );
+    // const data = {
+    //   params: {
+    //     keyTidOutsid: { "1345442199065381079": "4315697655846" },
+    //     tidSelectedOids: {
+    //       "1345442199065381079": [
+    //         "1345442199066381079",
+    //         "1345442199067381079",
+    //       ],
+    //     },
+    //     keyTidCtids: { "1345442199065381079": ["1345442199065381079"] },
+    //     tableType: 0,
+    //     templateId: 925325,
+    //     templateName: "韵达_一联_2",
+    //     templateType: 1,
+    //     templateKind: 1,
+    //     expressId: 50,
+    //     expressName: "韵达",
+    //     packageType: 0,
+    //     tidRowIndex: { "1345442199065381079": 1 },
+    //     tidJiazhuangMap: { "1345442199065381079": "" },
+    //     tidThreeplMap: { "1345442199065381079": {} },
+    //     sendType: "offline",
+    //   },
+    //   bodyRequest: 1,
+    // };
+    // $.ajax({
+    //   url: "http://tc1.woda.com/printSend.do?m=send",
+    //   type: "POST",
+    //   contentType: "application/json; charset=utf-8",
+    //   dataType: "json",
+    //   data: JSON.stringify(data),
+    // });
+    // }, 5000);
+    this.onInitProxy();
   },
 };
 </script>
