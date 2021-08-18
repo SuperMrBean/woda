3baa8c631ca6cdbfc179c7b960985b5ac5f6254d
孙志斌 <sunzhibin@corp.netease.com>
2021-08-18 20:09:26 +0800

    feat: 追风图

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="table" v-if="show">
     <span class="close" @click="onClose">x</span>
-    <div>{{ tradeList.length }}</div>
+    <div>{{ list.length }}</div>
   </div>
   <el-button class="btnHide" @click="onOpen" v-else type="primary"
     >open</el-button
@@ -16,7 +16,7 @@ export default {
   data: function() {
     return {
       show: false,
-      tradeList: [],
+      list: [],
     };
   },
   methods: {
@@ -35,119 +35,78 @@ export default {
         onResponse: (response, handler) => {
           const { config = {}, response: data = {} } = response || {};
           const { url = "" } = config || {};
-          if (url.indexOf("/printSend.do?m=queryTrades") > -1) {
-            const { tradeList = [] } = JSON.parse(data).data || {};
-            this.tradeList = tradeList;
-            //给容器加上订单号,之后方便操作
-            if (tradeList.length > 0) {
-              setTimeout(() => {
-                $(".table-row.row-expand").attr("id", function() {
-                  //将订单货物的div也设置一个id,方便后续操作
-                  var searchOrderId = $(this)
-                    .next()
-                    .find(".table-column.trade-detail-column1:first")
-                    .find("a:first")
-                    .html();
-                  $(this)
-                    .next()
-                    .attr("id", "orderSku_" + searchOrderId);
-                  //将orderid属性等于纯文件
-                  $(this).attr("orderid", searchOrderId);
-                  return "orderInfo_" + searchOrderId;
-                });
-              }, 1000);
-            }
+          if (url.indexOf("/trade-pack/find-pack-list") > -1) {
+            const { content = [] } = JSON.parse(data) || {};
+            this.list = content;
           }
           handler.next(response);
         },
       });
     },
-    onGetOrderDetail(orderId) {
-      unProxy();
-      setTimeout(() => {
-        const id = `#orderInfo_${orderId}`;
-        console.log(
-          $(id)
-            .find("i[title='明文显示收件信息']")
-            .click()
-        );
-      }, 2000);
-    },
     onOpen() {
-      this.onGetOrderDetail("1688776129161565806");
-      // this.show = true;
+      this.show = true;
     },
     onClose() {
       this.show = false;
     },
   },
   mounted() {
-    // setTimeout(() => {
-    // $.get("http://8.134.72.1/testCORS.php", (res) => {
-    //   console.log(res);
-    // });
-    // $.post(
-    //   "http://tc1.woda.com/printSend.do?m=batchSaveOutsid",
-    //   {
-    //     expressId: "50",
-    //     tableType: "0",
-    //     keyTidCtids: JSON.stringify({
-    //       "1345442199065381079": ["1345442199065381079"],
-    //     }),
-    //     expressTemplateId: "934771",
-    //     expressTemplateName: "韵达手动填单号",
-    //     expressTemplateKind: "0",
-    //     keyTidOutsid: JSON.stringify({
-    //       "1345442199065381079": "4315697655846",
-    //     }),
-    //     tidSelectedOids: JSON.stringify({
-    //       "1345442199065381079": [
-    //         "1345442199066381079",
-    //         "1345442199067381079",
-    //       ],
-    //     }),
-    //   },
-    //   (res) => {
-    //     let originData = list.find((item) => item.keyTid === id);
-    //     const { data = {} } = JSON.parse(res) || {};
-    //     Object.assign(originData, { ...data[id] });
-    //     ids.shift();
-    //     getItemData(ids);
-    //   }
-    // );
-    // const data = {
-    //   params: {
-    //     keyTidOutsid: { "1345442199065381079": "4315697655846" },
-    //     tidSelectedOids: {
-    //       "1345442199065381079": [
-    //         "1345442199066381079",
-    //         "1345442199067381079",
-    //       ],
-    //     },
-    //     keyTidCtids: { "1345442199065381079": ["1345442199065381079"] },
-    //     tableType: 0,
-    //     templateId: 925325,
-    //     templateName: "韵达_一联_2",
-    //     templateType: 1,
-    //     templateKind: 1,
-    //     expressId: 50,
-    //     expressName: "韵达",
-    //     packageType: 0,
-    //     tidRowIndex: { "1345442199065381079": 1 },
-    //     tidJiazhuangMap: { "1345442199065381079": "" },
-    //     tidThreeplMap: { "1345442199065381079": {} },
-    //     sendType: "offline",
-    //   },
-    //   bodyRequest: 1,
-    // };
-    // $.ajax({
-    //   url: "http://tc1.woda.com/printSend.do?m=send",
-    //   type: "POST",
-    //   contentType: "application/json; charset=utf-8",
-    //   dataType: "json",
-    //   data: JSON.stringify(data),
-    // });
-    // }, 5000);
+    setTimeout(() => {
+      // $.get("http://8.134.72.1/testCORS.php", (res) => {
+      //   console.log(res);
+      // });
+      // $.post(
+      //   "http://tc1.woda.com/printSend.do?m=batchSaveOutsid",
+      //   {
+      //     expressId: "50",
+      //     tableType: "0",
+      //     keyTidCtids: JSON.stringify({
+      //       "1345442199065381079": ["1345442199065381079"],
+      //     }),
+      //     expressTemplateId: "934771",
+      //     expressTemplateName: "韵达手动填单号",
+      //     expressTemplateKind: "0",
+      //     keyTidOutsid: JSON.stringify({
+      //       "1345442199065381079": "4315697655846",
+      //     }),
+      //     tidSelectedOids: JSON.stringify({
+      //       "1345442199065381079": [
+      //         "1345442199066381079",
+      //         "1345442199067381079",
+      //       ],
+      //     }),
+      //   },
+      //   (res) => {
+      //     let originData = list.find((item) => item.keyTid === id);
+      //     const { data = {} } = JSON.parse(res) || {};
+      //     Object.assign(originData, { ...data[id] });
+      //     ids.shift();
+      //     getItemData(ids);
+      //   }
+      // );
+      // const data = {
+      //   encryptedAddressList: [
+      //     {
+      //       objectId: "0",
+      //       tidList: ["1375708800108342399"],
+      //       receiverName: "陆**",
+      //       receiverMobile: "*******2559",
+      //       receiverAddress: "兰*街道**小区*区**号楼",
+      //       oaid:
+      //         "1fIsMXCesa3podkGKmoaG551FoRlPHBwzojw57icDxxf6D1Rr369f7H98aj0ezpfNpYg6Q7m",
+      //     },
+      //   ],
+      //   decryptAuthType: "SHOP",
+      //   targetId: 1820,
+      // };
+      // $.ajax({
+      //   url: "https://zft.topchitu.com/api/security/batch-decrypt-address",
+      //   type: "POST",
+      //   contentType: "application/json; charset=utf-8",
+      //   dataType: "json",
+      //   data: JSON.stringify(data),
+      // });
+    }, 10000);
     this.onInitProxy();
   },
 };
